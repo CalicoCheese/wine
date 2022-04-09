fec55d14c8763e6f826a70c9dc1b83bf7ca68325
hurrhnn <20sunrin022@sunrint.hs.kr>
2022-04-09 22:54:50 +0900

    unixlib: Removed duplicate of exception handling code.
    Signed-off-by: hurrhnn <20sunrin022@sunrint.hs.kr>

--- include/wine/unixlib.h
+++ include/wine/unixlib.h
@@ -39,49 +39,6 @@ extern int ntdll_wcsicmp( const WCHAR *str1, const WCHAR *str2 );
 extern int ntdll_wcsnicmp( const WCHAR *str1, const WCHAR *str2, int n );
 extern NTSTATUS ntdll_init_syscalls( ULONG id, SYSTEM_SERVICE_TABLE *table, void **dispatcher );
 
-/* exception handling */
-
-#ifdef __i386__
-typedef struct { int reg[16]; } __wine_jmp_buf;
-#elif defined(__x86_64__)
-typedef struct { DECLSPEC_ALIGN(16) struct { unsigned __int64 Part[2]; } reg[16]; } __wine_jmp_buf;
-#elif defined(__arm__)
-typedef struct { int reg[28]; } __wine_jmp_buf;
-#elif defined(__aarch64__)
-typedef struct { __int64 reg[24]; } __wine_jmp_buf;
-#else
-typedef struct { int reg; } __wine_jmp_buf;
-#endif
-
-extern int __cdecl __attribute__ ((__nothrow__,__returns_twice__)) __wine_setjmpex( __wine_jmp_buf *buf,
-                                                   EXCEPTION_REGISTRATION_RECORD *frame );
-extern void DECLSPEC_NORETURN __cdecl __wine_longjmp( __wine_jmp_buf *buf, int retval );
-extern void ntdll_set_exception_jmp_buf( __wine_jmp_buf *jmp );
-
-#define __TRY \
-    do { __wine_jmp_buf __jmp; \
-         int __first = 1; \
-         for (;;) if (!__first) \
-         { \
-             do {
-
-#define __EXCEPT \
-             } while(0); \
-             ntdll_set_exception_jmp_buf( NULL ); \
-             break; \
-         } else { \
-             if (__wine_setjmpex( &__jmp, NULL )) { \
-                 do {
-
-#define __ENDTRY \
-                 } while (0); \
-                 break; \
-             } \
-             ntdll_set_exception_jmp_buf( &__jmp ); \
-             __first = 0; \
-         } \
-    } while (0);
-
 NTSTATUS WINAPI KeUserModeCallback( ULONG id, const void *args, ULONG len, void **ret_ptr, ULONG *ret_len );
 
 /* wide char string functions */
