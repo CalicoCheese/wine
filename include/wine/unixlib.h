4a7f3d05adf2f051d8954fba2d3ac480b3886b46
hurrhnn <20sunrin022@sunrint.hs.kr>
2022-04-10 23:35:22 +0900

    unixlib: Added missing a lot of symbols.
    Signed-off-by: hurrhnn <20sunrin022@sunrint.hs.kr>

--- include/wine/unixlib.h
+++ include/wine/unixlib.h
@@ -23,6 +23,9 @@
 
 #include <wine/winheader_enter.h>
 
+extern USHORT * HOSTPTR uctable DECLSPEC_HIDDEN;
+extern USHORT * HOSTPTR lctable DECLSPEC_HIDDEN;
+
 typedef NTSTATUS (*unixlib_entry_t)( void *args );
 typedef UINT64 unixlib_handle_t;
 
@@ -35,35 +38,51 @@ extern const char *ntdll_get_build_dir(void);
 extern const char *ntdll_get_data_dir(void);
 extern DWORD ntdll_umbstowcs( const char * HOSTPTR src, DWORD srclen, WCHAR * HOSTPTR dst, DWORD dstlen );
 extern int ntdll_wcstoumbs( const WCHAR * HOSTPTR src, DWORD srclen, char * HOSTPTR dst, DWORD dstlen, BOOL strict );
-extern int ntdll_wcsicmp( const WCHAR *str1, const WCHAR *str2 );
-extern int ntdll_wcsnicmp( const WCHAR *str1, const WCHAR *str2, int n );
 extern NTSTATUS ntdll_init_syscalls( ULONG id, SYSTEM_SERVICE_TABLE *table, void **dispatcher );
 
 NTSTATUS WINAPI KeUserModeCallback( ULONG id, const void *args, ULONG len, void **ret_ptr, ULONG *ret_len );
 
+static inline WCHAR ntdll_towupper( WCHAR ch )
+{
+    return ch + uctable[uctable[uctable[ch >> 8] + ((ch >> 4) & 0x0f)] + (ch & 0x0f)];
+}
+
+static inline WCHAR ntdll_towlower( WCHAR ch )
+{
+    return ch + lctable[lctable[lctable[ch >> 8] + ((ch >> 4) & 0x0f)] + (ch & 0x0f)];
+}
+
+static inline WCHAR *ntdll_wcsupr( WCHAR * str )
+{
+    WCHAR *ret;
+    for (ret = str; *str; str++) *str = ntdll_towupper(*str);
+    return ret;
+}
+
 /* wide char string functions */
 
-static inline size_t ntdll_wcslen( const WCHAR *str )
+static inline size_t ntdll_wcslen( const WCHAR * HOSTPTR str )
 {
-    const WCHAR *s = str;
+    const WCHAR * HOSTPTR s = str;
     while (*s) s++;
     return s - str;
 }
 
-static inline WCHAR *ntdll_wcscpy( WCHAR *dst, const WCHAR *src )
+/* 32on64 FIXME: If anyone uses the retval we need to make this overloadble. */
+static inline WCHAR * HOSTPTR ntdll_wcscpy( WCHAR * HOSTPTR dst, const WCHAR * HOSTPTR src )
 {
-    WCHAR *p = dst;
+    WCHAR * HOSTPTR p = dst;
     while ((*p++ = *src++));
     return dst;
 }
 
-static inline WCHAR *ntdll_wcscat( WCHAR *dst, const WCHAR *src )
+static inline WCHAR * HOSTPTR ntdll_wcscat( WCHAR * HOSTPTR dst, const WCHAR * HOSTPTR src )
 {
     ntdll_wcscpy( dst + ntdll_wcslen(dst), src );
     return dst;
 }
 
-static inline int ntdll_wcscmp( const WCHAR *str1, const WCHAR *str2 )
+static inline int ntdll_wcscmp( const WCHAR * HOSTPTR str1, const WCHAR * HOSTPTR str2 )
 {
     while (*str1 && (*str1 == *str2)) { str1++; str2++; }
     return *str1 - *str2;
@@ -82,31 +101,58 @@ static inline WCHAR *ntdll_wcschr( const WCHAR *str, WCHAR ch )
     return NULL;
 }
 
-static inline WCHAR *ntdll_wcsrchr( const WCHAR *str, WCHAR ch )
+#ifdef __i386_on_x86_64__
+static inline WCHAR * HOSTPTR ntdll_wcschr( const WCHAR * HOSTPTR str, WCHAR ch ) __attribute__((overloadable))
 {
-    WCHAR *ret = NULL;
-    do { if (*str == ch) ret = (WCHAR *)(ULONG_PTR)str; } while (*str++);
+    do { if (*str == ch) return (WCHAR * HOSTPTR)(ULONGLONG)str; } while (*str++);
+    return NULL;
+}
+#endif
+
+static inline WCHAR * HOSTPTR ntdll_wcsrchr( const WCHAR * HOSTPTR str, WCHAR ch )
+{
+    WCHAR * HOSTPTR ret = NULL;
+    do { if (*str == ch) ret = (WCHAR * HOSTPTR)(ULONG_HOSTPTR)str; } while (*str++);
     return ret;
 }
 
-static inline WCHAR *ntdll_wcspbrk( const WCHAR *str, const WCHAR *accept )
+static inline WCHAR * HOSTPTR ntdll_wcspbrk( const WCHAR * HOSTPTR str, const WCHAR * HOSTPTR accept )
 {
-    for ( ; *str; str++) if (ntdll_wcschr( accept, *str )) return (WCHAR *)(ULONG_PTR)str;
+    for ( ; *str; str++) if (ntdll_wcschr( accept, *str )) return (WCHAR * HOSTPTR)(ULONG_HOSTPTR)str;
     return NULL;
 }
 
-static inline SIZE_T ntdll_wcsspn( const WCHAR *str, const WCHAR *accept )
+static inline SIZE_T ntdll_wcsspn( const WCHAR * HOSTPTR str, const WCHAR * HOSTPTR accept )
 {
-    const WCHAR *ptr;
+    const WCHAR * HOSTPTR ptr;
     for (ptr = str; *ptr; ptr++) if (!ntdll_wcschr( accept, *ptr )) break;
     return ptr - str;
 }
 
-static inline SIZE_T ntdll_wcscspn( const WCHAR *str, const WCHAR *reject )
+static inline SIZE_T ntdll_wcscspn( const WCHAR * HOSTPTR str, const WCHAR * HOSTPTR reject )
 {
-    const WCHAR *ptr;
+    const WCHAR * HOSTPTR ptr;
     for (ptr = str; *ptr; ptr++) if (ntdll_wcschr( reject, *ptr )) break;
     return ptr - str;
+}
+
+static inline int ntdll_wcsicmp( const WCHAR * HOSTPTR str1, const WCHAR * HOSTPTR str2 )
+{
+    int ret;
+    for (;;)
+    {
+        if ((ret = ntdll_towupper( *str1 ) - ntdll_towupper( *str2 )) || !*str1) return ret;
+        str1++;
+        str2++;
+    }
+}
+
+static inline int ntdll_wcsnicmp( const WCHAR * HOSTPTR str1, const WCHAR * HOSTPTR str2, int n )
+{
+    int ret;
+    for (ret = 0; n > 0; n--, str1++, str2++)
+        if ((ret = ntdll_towupper(*str1) - ntdll_towupper(*str2)) || !*str1) break;
+    return ret;
 }
 
 #define wcslen(str)        ntdll_wcslen(str)
@@ -121,6 +167,9 @@ static inline SIZE_T ntdll_wcscspn( const WCHAR *str, const WCHAR *reject )
 #define wcscspn(str,rej)   ntdll_wcscspn(str,rej)
 #define wcsicmp(s1, s2)    ntdll_wcsicmp(s1,s2)
 #define wcsnicmp(s1, s2,n) ntdll_wcsnicmp(s1,s2,n)
+#define wcsupr(str)        ntdll_wcsupr(str)
+#define towupper(c)        ntdll_towupper(c)
+#define towlower(c)        ntdll_towlower(c)
 
 #endif /* WINE_UNIX_LIB */
 
